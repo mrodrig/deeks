8e9bfbd47b176596480afab26e467864272ca0ff
mrodrig <rodrigues.mi@northeastern.edu>
2023-02-11 23:51:14 +0000

    chore(build): latest built files

--- lib/types.d.ts
+++ lib/types.d.ts
@@ -1,6 +1,6 @@
-export type DeeksOptions = {
+export interface DeeksOptions {
     expandArrayObjects: boolean;
     ignoreEmptyArraysWhenExpanding: boolean;
     escapeNestedDots: boolean;
     ignoreEmptyArrays: boolean;
-};
+}
